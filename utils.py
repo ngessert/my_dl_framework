494679de115f68bb3eb0601b95bbc989e4f68df8
Gessert <nils.gessert@tuhh.de>
2021-07-18 23:18:47 +0200

    Start the setup

--- utils.py
+++ utils.py
@@ -1,4 +1,25 @@
 """
 Training utility stuff
 """
+from typing import Dict
+from torch.utils.data import Dataset
 
+
+def get_dataset(config: Dict) -> Dataset:
+    if config["dataset_type"] == "RSNAChallenge":
+        return RSNAChallengeDataset(config=config)
+    else:
+        raise ValueError(f'Unknown dataset type {config["dataset_type"]}')
+
+
+class RSNAChallengeDataset(Dataset):
+    def __init__(self, config: Dict):
+        self.config = config
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        image = None
+        label = None
+        return image, label
